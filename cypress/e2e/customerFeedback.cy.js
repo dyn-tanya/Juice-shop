08325c60f730f554ad59fcdfa64b9732f3569900
dyn-tanya <tanya.dyn2992@gmail.com>
2023-06-11 16:58:49 +0300

    fix import

--- cypress/e2e/customerFeedback.cy.js
+++ cypress/e2e/customerFeedback.cy.js
@@ -1,5 +1,5 @@
 import feedBackPage from '../support/pages/FeedBackPage'
-import HomePage from '../support/pages/HomePage';
+import homePage from '../support/pages/HomePage';
 import { captchaResult } from '../support/pages/CaptchaFunction'
 import user from '../fixtures/user.json'
 
@@ -8,7 +8,7 @@ describe('Feedback form', () => {
     it('Filling out the feedback form', () => {
 
         feedBackPage.visit();
-        HomePage.getWelcomeBanner().click();
+        homePage.getWelcomeBanner().click();
         feedBackPage.getCommentField().type(user.commentUser)
 
         feedBackPage.getSlider().click()
